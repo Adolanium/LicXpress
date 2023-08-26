052eca8fda8b4b99305c308cb1f06b9c0048b5a0
Adolanium <Adolanium@users.noreply.github.com>
2023-08-26 09:43:31 +0300

    Revert "Replace Selenium with Playwright"
    This reverts commit c854ecf60c2ce88103d478a84aeca12e045049c7.

--- LicXpress/Form1.cs
+++ LicXpress/Form1.cs
@@ -1,41 +1,85 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 using System;
 using System.IO;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using Microsoft.Playwright;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+using WebDriverManager.Helpers;
 
 namespace LicXpress
 {
     public partial class Form1 : Form
     {
         private const string BASE_URL = "https://my.solidworks.com/xpress";
-        private readonly Configuration _config;
+        private const int DEFAULT_WAIT_TIME = 10;
 
         public Form1()
         {
             InitializeComponent();
-            _config = new Configuration("credentials.txt");
         }
 
-        internal static async Task NavigateToBaseURLAsync(IPage page)
+        public static IWebDriver CreateChromeDriver()
         {
-            await page.GotoAsync(BASE_URL);
+            ChromeOptions options = new ChromeOptions();
+            options.AddArguments("headless");
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService();
+            service.SuppressInitialDiagnosticInformation = true;
+            service.HideCommandPromptWindow = true;
+            return new ChromeDriver(service, options);
         }
 
-        internal static async Task LoginAsync(IPage page, Configuration config)
+        public static WebDriverWait GetWebDriverWait(IWebDriver driver, int seconds = DEFAULT_WAIT_TIME)
+        {
+            return new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+        }
+
+        static void NavigateToBaseURL(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl(BASE_URL);
+        }
+
+        static Tuple<string, string> ReadCredentials()
         {
             try
             {
-                string username = config.Username;
-                string password = config.Password;
+                string path = "credentials.txt";
+                string[] lines = File.ReadAllLines(path);
 
-                await page.ClickAsync("#footer_tc_privacy_button_3");
-                await page.ClickAsync("text=Log In");
-                await page.ClickAsync("//*[@id=\"dsxLoginWrapper\"]/div[3]/div[2]/a[1]");
+                string username = lines[0].Split('=')[1].Trim();
+                string password = lines[1].Split('=')[1].Trim();
 
-                await page.FillAsync("[name='username']", username);
-                await page.FillAsync("[name='password']", password);
-                await page.PressAsync("[name='password']", "Enter");
+                return new Tuple<string, string>(username, password);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show($"Failed to read credentials: {e.Message}");
+                return new Tuple<string, string>(string.Empty, string.Empty);
+            }
+        }
+
+
+        static void Login(IWebDriver driver)
+        {
+            try
+            {
+                var credentials = ReadCredentials();
+                string username = credentials.Item1;
+                string password = credentials.Item2;
+
+                var wait = GetWebDriverWait(driver);
+                wait.Until(ExpectedConditions.ElementIsVisible(By.Id("footer_tc_privacy_button_3"))).Click();
+                wait.Until(ExpectedConditions.ElementIsVisible(By.LinkText("Log In"))).Click();
+                wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"dsxLoginWrapper\"]/div[3]/div[2]/a[1]"))).Click();
+
+                var emailField = wait.Until(ExpectedConditions.ElementIsVisible(By.Name("username")));
+                emailField.SendKeys(username);
+
+                var passwordField = driver.FindElement(By.Name("password"));
+                passwordField.SendKeys(password);
+                passwordField.SendKeys(OpenQA.Selenium.Keys.Enter);
             }
             catch (Exception e)
             {
@@ -43,14 +87,21 @@ namespace LicXpress
             }
         }
 
-        internal static async Task ExtractSerialAsync(IPage page, string _serial, string _version, string _product)
+        static void ExtractSerial(IWebDriver driver, string _serial, string _version, string _product)
         {
             try
             {
-                await page.FillAsync("#serial", _serial);
-                await page.SelectOptionAsync("#version", _version);
-                await page.SelectOptionAsync("#product", _product);
-                await page.ClickAsync("#serialSubmit");
+                WebDriverWait waitSerial = GetWebDriverWait(driver, 25);
+                waitSerial.Until(ExpectedConditions.ElementIsVisible(By.Id("serial")));
+
+                driver.FindElement(By.Id("serial")).SendKeys(_serial);
+                SelectElement version = new SelectElement(driver.FindElement(By.Id("version")));
+                version.SelectByText(_version);
+
+                SelectElement product = new SelectElement(driver.FindElement(By.Id("product")));
+                product.SelectByText(_product);
+
+                driver.FindElement(By.Id("serialSubmit")).Click();
             }
             catch (Exception e)
             {
@@ -58,39 +109,25 @@ namespace LicXpress
             }
         }
 
-        private async void generateBtn_Click(object sender, EventArgs e)
+        private void generateBtn_Click(object sender, EventArgs e)
         {
-            try
-            {
-                string text = CleanSerialNumber(serialTextBox.Text);
-                serialTextBox.Text = text;
+            string text = serialTextBox.Text.Replace(" ", "").Replace("\r", "").Replace("\"", "");
+            serialTextBox.Text = text;
 
-                string _version = versionListBox.SelectedItem.ToString();
-                string _product = productListBox.SelectedItem.ToString();
+            string _version = versionListBox.SelectedItem.ToString();
+            string _product = productListBox.SelectedItem.ToString();
 
-                using var playwright = await Playwright.CreateAsync();
-                await using var browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = true });
+            new DriverManager().SetUpDriver(new ChromeConfig(), VersionResolveStrategy.MatchingBrowser);
+            IWebDriver driver = CreateChromeDriver();
+            NavigateToBaseURL(driver);
+            Login(driver);
+            ExtractSerial(driver, text, _version, _product);
 
-                var context = await browser.NewContextAsync();
-                var page = await context.NewPageAsync();
-
-                await NavigateToBaseURLAsync(page);
-                await LoginAsync(page, _config);
-                await ExtractSerialAsync(page, text, _version, _product);
-
-                string xpressCode = await page.InnerTextAsync("//*[@id=\"expressmail\"]/div[1]/table/tbody/tr/td[2]/span/b");
-                codeTextBox.Text = xpressCode;
-                MessageBox.Show("Done! The code for " + _product + " " + _version + " is: " + xpressCode);
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"An error occurred: {ex.Message}");
-            }
-        }
-
-        private static string CleanSerialNumber(string input)
-        {
-            return input.Replace(" ", "").Replace("\r", "").Replace("\"", "");
+            WebDriverWait waitActivationCode = GetWebDriverWait(driver);
+            string xpressCode = waitActivationCode.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@id=\"expressmail\"]/div[1]/table/tbody/tr/td[2]/span/b"))).Text;
+            codeTextBox.Text = xpressCode;
+            MessageBox.Show("Done! The code for " + _product + " " + _version + " is: " + xpressCode);
+            driver.Quit();
         }
     }
 }
